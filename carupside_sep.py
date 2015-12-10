71b5515afb1c43e44665d023660f40eb509ed0dd
glorylife <glorylife00aoi@gmail.com>
2015-12-10 11:06:19 +0700

    ทำกราฟสองเส้นให้ข้อมูลถูกต้องคร่าวๆ

--- carupside_sep.py
+++ carupside_sep.py
@@ -1,19 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
-x = [0,1,2,3,4,5,6]
-y = [2.9,3.1,3.4,3.5,3.6,4,0]
+
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
+
 ax1.set_title('graph')
 ax1.set_xlabel('year201X')
-ax1.set_ylabel('value')
+ax1.set_ylabel('value()')
+
+x = [0,1,2,3,4,5]
+y = [2.81,2.99,3.18,3.42,3.56,3.65]
 ax1.plot(x,y,c='g',label='stock car')
-a = [0,1,2,3,4,5,6]
-b = [1.7,1.8,2.4,1.3,0.9,1,0]
-ax1.plot(a,b,c='r',label='diff car')
-g = [0,1,2,3,4,5,6]
-h = [2,2,2,2,2,2,4]
-ax1.plot(g,h,c='b',label='try')
-leg = ax1.legend(loc='upper left')
+for x,y in zip(x,y):
+    plt.text(x, y, "%.2f" %y, ha='center',va='bottom')
+w = [5,6]
+z = [3.65, 4] # z[1] ยังไม่ใช่ค่าจริงๆ
+ax1.plot(w,z,c='r',label='future stock car')
+for x,y in zip(w,z):
+    plt.text(x, y, "%.2f" %y, ha='center',va='bottom')
+    
+a = [1,2,3,4,5]
+b = [1.7,1.8,2.4,1.3,0.9]
+ax1.plot(a,b,c='b',label='diff car')
+for x,y in zip(a,b):
+    plt.text(x, y, "%.2f" %y, ha='center',va='bottom')
+g = [5,6]
+h = [0.9,1.2] #h[1] ยังไม่ใช่ค่าจริงๆ
+ax1.plot(g,h,c='r',label='future diff car')
+leg = ax1.legend(loc='lower left')
 plt.show()
-print("end")
+
